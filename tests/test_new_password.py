99c3be3b1e0c21452f70f5891d79fb54cd73a05f
zaeso <zaertzaerty0@gmail.com>
2024-11-23 11:02:14 +0300

    new test

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,37 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    for length in [5, 10, 20, 50]:
+        password = generate_password(length)
+        assert len(password) == length
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_password_uniqueness():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(20)  # Используем достаточную длину, чтобы снизить вероятность совпадения
+    password2 = generate_password(20)
+    assert password1 != password2
+
+
+def test_password_contains_lowercase():
+    """Тест, что пароль содержит хотя бы одну строчную букву"""
+    password = generate_password(20)
+    assert any(c.islower() for c in password)
+
+
+def test_password_contains_uppercase():
+    """Тест, что пароль содержит хотя бы одну заглавную букву"""
+    password = generate_password(20)
+    assert any(c.isupper() for c in password)
+
+
+def test_password_contains_digit():
+    """Тест, что пароль содержит хотя бы одну цифру"""
+    password = generate_password(20)
+    assert any(c.isdigit() for c in password)
+
+def test_password_contains_punctuation():
+    """Тест, что пароль содержит хотя бы один знак пунктуации"""
+    password = generate_password(20)
+    assert any(c in string.punctuation for c in password)
